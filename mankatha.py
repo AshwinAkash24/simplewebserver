6a36faaddd1ba5fd02d192bafd3a63018df18e58
AshwinAkash24 <ashwinakash242005@gmail.com>
2024-03-12 11:47:21 +0530

    Success

---
+++ mankatha.py
@@ -0,0 +1,61 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content="""
+<html>
+<head>
+<title>Software companies</title>
+</head>
+<body bgcolor="aqua">
+<table align="center" border="3" bordercolor="white" cellspacing="3" cellpadding="4">
+<caption><h3>Top Five Revenue Generating Software Companies</h3></caption>
+<tr>
+<th>Rank</th>
+<th>Company</th>
+<th>Sales</th>
+<th>Nationality</th>
+</tr>
+<tr>
+<td>1</td>
+<td>Microsoft</td>
+<td>57.9</td>
+<td>USA</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Oracle</td>
+<td>21.0</td>
+<td>USA</td>
+</tr>
+<tr>
+<td>3</td>
+<td>SAP</td>
+<td>16.1</td>
+<td>GERMANY</td>
+</tr>
+<tr>
+<td>4</td>
+<td>Computer Associates</td>
+<td>4.2</td>
+<td>USA</td>
+</tr>
+<tr>
+<td>5</td>
+<td>Adobe</td>
+<td>3.4</td>
+<td>USA</td>
+</tr>
+</table>
+</body>
+</html>
+
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
